6b26e95b249f8a014c60b9fcdd8fd603710f838c
Pedro Cella <pedrovscella@gmail.com>
2021-01-19 08:01:42 -0300

    Adicionando complete and delete task

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -17,7 +17,7 @@ function App() {
       todos={todos} 
       setTodos={setTodos} 
       setInputText={setInputText} />
-      <TodoList />
+      <TodoList setTodos={setTodos} todos={todos} />
     </div>
   );
 }
